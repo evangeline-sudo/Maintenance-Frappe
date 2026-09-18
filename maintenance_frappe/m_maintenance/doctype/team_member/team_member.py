2a463680e7e2a40d87d71f69636a6ec5ddaad508
Evangeline <evangelinerajeswari@gmail.com>
2026-09-18 18:32:47 +0530

    Rebview changes-1

---
+++ maintenance_frappe/m_maintenance/doctype/team_member/team_member.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, Evangeline and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TeamMember(Document):
+	pass
